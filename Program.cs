4e8133dfa84cb52157bea8f6378f60457f5a9fb0
soroka <sorokindv88@mail.ru>
2022-11-14 21:30:34 +0300

    seminar 4

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+int FindSum(int number)
+{
+    int sum = 0;
+
+    for(int current = 1; current <= number; current++)
+        sum += current;
+
+    return sum;
+}
+
+Console.Write("Input a number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Sum of numbers between 1 and {a} is {FindSum(a)}");
+*/
+
